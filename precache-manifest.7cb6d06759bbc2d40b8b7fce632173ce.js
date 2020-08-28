0c44e74c483dc432e9751d555df817347c350994
Gauravdada <gaurav.mazumder1997@gmail.com>
2020-08-28 20:55:41 +0530

    Updates

---
+++ precache-manifest.7cb6d06759bbc2d40b8b7fce632173ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85cd8ca76d1ec10fc0070a2db3e167c4",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "d34208cf16b43e7afa0a",
+    "url": "/Covid-19-Tracker/static/css/main.366825bd.chunk.css"
+  },
+  {
+    "revision": "8851bb1053699877dc61",
+    "url": "/Covid-19-Tracker/static/js/2.c066e1f5.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid-19-Tracker/static/js/2.c066e1f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d34208cf16b43e7afa0a",
+    "url": "/Covid-19-Tracker/static/js/main.9bfa1eac.chunk.js"
+  },
+  {
+    "revision": "cc473f0babe70de1682d",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.9c533d51.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-Tracker/static/media/img.d7265326.png"
+  }
+]);
